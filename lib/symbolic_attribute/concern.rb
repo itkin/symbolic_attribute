b19e36c7dd645cc94cd395f21d8be619580c9991
itkin <nicolas.papon@webflows.fr>
2015-09-26 02:49:00 +0200

    store the choices option into a class attribute and add validation

--- lib/symbolic_attribute/concern.rb
+++ lib/symbolic_attribute/concern.rb
@@ -2,6 +2,7 @@ require 'active_support/concern'
 require 'active_support/core_ext/hash/keys'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/class/attribute'
 
 module SymbolicAttribute
   module Concern
@@ -15,14 +16,12 @@ module SymbolicAttribute
           val.to_sym unless val.blank?
         end
 
+
         if opts.symbolize_keys!.key?(:choices)
-          opts[:choices].map!(&:to_sym)
-          metaclass = class << self; self; end
-          metaclass.instance_eval do
-            define_method :"available_#{attr.to_s.pluralize}" do
-              opts[:choices]
-            end
-          end
+          class_attr_name= attr.to_s.pluralize
+          class_attribute class_attr_name, :instance_reader => false, :instance_writer => false
+          send "#{class_attr_name}=", opts.delete(:choices).map(&:to_sym)
+          validates attr, opts.merge(:inclusion => { :in => proc{|instance| instance.class.send(class_attr_name)} })
         end
 
       end
--- spec/symbolic_attribute_spec.rb
+++ spec/symbolic_attribute_spec.rb
@@ -1,29 +1,38 @@
 require 'spec_helper'
+require 'active_model'
 
 describe SymbolicAttribute do
 
-  class TargetClass
+  class SubjectClass
     attr_accessor :role
+    include ActiveModel::Validations
+    include SymbolicAttribute::Concern
+
     def read_attribute(attr)
       instance_variable_get :"@#{attr}"
     end
-    include SymbolicAttribute::Concern
-    symbolic_attribute :role, :choices => [:buyer, :seller]
+    symbolic_attribute :role, :choices => [:buyer, :seller], :allow_nil => true
   end
 
-  let(:target) { TargetClass.new }
+  let(:subject_class) { SubjectClass }
+  subject { subject_class.new }
 
   it 'has a version number' do
     expect(SymbolicAttribute::VERSION).not_to be nil
   end
 
   it 'reader symbolizes attribute' do
-    target.role = "fuu"
-    expect(target.role).to eq :fuu
+    subject.role = "fuu"
+    expect(subject.role).to eq :fuu
   end
 
   it 'generates a dynamic class method listing choices' do
-    expect(TargetClass.available_roles).to eq [:buyer, :seller]
+    expect(subject_class.roles).to eq [:buyer, :seller]
   end
 
+  it 'validates inclusion ' do
+    expect{
+      subject.role = "fuu"
+    }.to change{subject.valid?}.from(true).to(false)
+  end
 end
